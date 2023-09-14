824ae6409ccb6ad981d6a671d77525ce813a2293
YcPeh <104683280+YcPeh@users.noreply.github.com>
2023-09-14 23:32:35 +0800

    done profile page (2.48.20)

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,4 +1,36 @@
-const Profile = () => {
-  return <div>Profile</div>;
+"use client";
+
+import Profile from "@components/Profile";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      console.log("response", response.json);
+      const data = await response.json();
+      console.log("data", data);
+      setPosts(data);
+    };
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = () => {};
+
+  const handleDelete = async () => {};
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
-export default Profile;
+export default MyProfile;
